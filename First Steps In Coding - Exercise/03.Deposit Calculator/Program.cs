d2bdee0e00ead42d8ba686c3077c4686c7250b40
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-04 09:56:59 +0200

    Add files via upload

---
+++ First Steps In Coding - Exercise/03.Deposit Calculator/Program.cs
@@ -0,0 +1,8 @@
+﻿double deposit = double.Parse(Console.ReadLine());
+int months = int.Parse(Console.ReadLine());
+double percentage = double.Parse(Console.ReadLine());
+
+double interest = deposit * percentage /100;
+double interestPerMonth = interest / 12;
+double sum = deposit + months * interestPerMonth;
+Console.WriteLine(sum);
