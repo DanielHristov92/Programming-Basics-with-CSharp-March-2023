d404097962f64e57c2e00030e575b521b2abc63f
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-04 13:07:35 +0200

    Add files via upload

---
+++ Conditional Statements - Exercise/07.Shopping/Program.cs
@@ -0,0 +1,27 @@
+﻿double budget = double.Parse(Console.ReadLine());
+int videoCards = int.Parse(Console.ReadLine());
+int processors = int.Parse(Console.ReadLine());
+int ram = int.Parse(Console.ReadLine());
+
+int videoCardsPrice = videoCards * 250;
+double processorsPrice = videoCardsPrice * 0.35;
+double ramPrice = videoCardsPrice * 0.10;
+double processorsCount = processors * processorsPrice;
+double ramCount = ram * ramPrice;
+double totalSum = videoCardsPrice + processorsCount + ramCount;
+
+if (videoCards > processors)
+{
+    totalSum -= totalSum * 0.15;
+}
+
+if (budget >= totalSum)
+{
+    double remainingMoney = budget - totalSum;
+    Console.WriteLine($"You have {remainingMoney:F2} leva left!");
+}
+if (totalSum > budget)
+{
+    double neededMoney = totalSum - budget;
+    Console.WriteLine($"Not enough money! You need {neededMoney:F2} leva more!");
+}
---
+++ Conditional Statements - Exercise/08.Lunch Break/Program.cs
@@ -0,0 +1,18 @@
+﻿string nameSeries = Console.ReadLine();
+int durationEpisode = int.Parse(Console.ReadLine());
+int durationBreak = int.Parse(Console.ReadLine());
+
+double timeForLunch = durationBreak / 8.0;
+double timeForBreak = durationBreak / 4.0;
+double timeLeft = durationBreak - timeForLunch - timeForBreak;
+
+if (timeLeft >= durationEpisode)
+{
+    double left = timeLeft - durationEpisode;
+    Console.WriteLine($"You have enough time to watch {nameSeries} and left with {Math.Ceiling(left)} minutes free time.");
+}
+else
+{
+    double neededTime = durationEpisode - timeLeft;
+    Console.WriteLine($"You don't have enough time to watch {nameSeries}, you need {Math.Ceiling(neededTime)} more minutes.");
+}
