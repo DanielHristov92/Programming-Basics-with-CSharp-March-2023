b1177241fc2e862997fd57ec52645449c99c9c33
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-04 13:03:47 +0200

    Add files via upload

---
+++ Conditional Statements - Exercise/01.Sum Seconds/Program.cs
@@ -0,0 +1,10 @@
+﻿int firstTime = int.Parse(Console.ReadLine());
+int sedondTime = int.Parse(Console.ReadLine());
+int thirdTime = int.Parse(Console.ReadLine());
+
+int totalTime = firstTime + sedondTime + thirdTime;
+
+int minutes = totalTime / 60;
+int  seconds = totalTime % 60;
+
+Console.WriteLine($"{minutes}:{seconds:D2}");
---
+++ Conditional Statements - Exercise/03.Time + 15 Minutes/Program.cs
@@ -0,0 +1,7 @@
+﻿int hour = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+int nextHour = (hour + (minutes + 15) / 60) % 24;
+int nextMinutes = (minutes + 15) % 60;
+
+Console.WriteLine($"{nextHour}:{nextMinutes:D2}");
