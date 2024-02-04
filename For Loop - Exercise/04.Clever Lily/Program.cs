f45c408aa7bb9ee4a081b8e89f8bf06913436ec5
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-04 17:40:10 +0200

    Add files via upload

---
+++ For Loop - Exercise/04.Clever Lily/Program.cs
@@ -0,0 +1,32 @@
+﻿int age = int.Parse(Console.ReadLine());
+double washingMachinePrice = double.Parse(Console.ReadLine());
+int toyPrice = int.Parse(Console.ReadLine());
+
+double money = 0;
+int toysCount = 0;
+int brotherMoney = 0;
+
+for (int i = 1; i <= age; i++)
+{
+    if (i % 2 != 0)
+    {
+        toysCount++;
+    }
+    else
+    {
+        money += (10 * i) / 2;
+        brotherMoney++;
+    }
+}
+
+double totalMoney = money + (toysCount * toyPrice) - brotherMoney;
+
+if (totalMoney >= washingMachinePrice)
+{
+    Console.WriteLine($"Yes! {totalMoney - washingMachinePrice:F2}");
+}
+else
+{
+    Console.WriteLine($"No! {washingMachinePrice - totalMoney:F2}");
+}
+    
---
+++ For Loop - Exercise/06.Oscars/Program.cs
@@ -0,0 +1,21 @@
+﻿string actorName = Console.ReadLine();
+double academyPoints = double.Parse(Console.ReadLine());
+int judgesCount = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < judgesCount; i++)
+{
+    string judgeName = Console.ReadLine();
+    double judgePoints = double.Parse(Console.ReadLine());
+
+    double currentPoints = (judgeName.Length * judgePoints) / 2.0;
+    academyPoints += currentPoints;
+
+    if (academyPoints > 1250.5)
+    {
+        Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {academyPoints:f1}!");
+        return;
+    }
+}
+
+double neededPoints = 1250.5 - academyPoints;
+Console.WriteLine($"Sorry, {actorName} you need {neededPoints:f1} more!");
