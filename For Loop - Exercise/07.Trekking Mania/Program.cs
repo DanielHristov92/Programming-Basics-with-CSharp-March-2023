9203be6ccfd9dd556de0a8594ad459c4ad3e19d6
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-04 17:40:59 +0200

    Add files via upload

---
+++ For Loop - Exercise/07.Trekking Mania/Program.cs
@@ -0,0 +1,39 @@
+﻿int groupCount = int.Parse(Console.ReadLine());
+
+double mussala = 0;
+double monblanc = 0;
+double kilimanjaro = 0;
+double k2 = 0;
+double everest = 0;
+
+for (int i = 0; i < groupCount; i++)
+{
+    int groupSize = int.Parse(Console.ReadLine());
+
+    if (groupSize <= 5)
+    {
+        mussala += groupSize;
+    }
+    else if (groupSize <= 12)
+    {
+        monblanc += groupSize;
+    }
+    else if (groupSize <= 25)
+    {
+        kilimanjaro += groupSize;
+    }
+    else if (groupSize <= 40)
+    {
+        k2 += groupSize;
+    }
+    else
+    {
+        everest += groupSize;
+    }
+}
+double total = mussala + monblanc + kilimanjaro + k2 + everest;
+Console.WriteLine($"{(mussala * 100.0 / total):f2}%");
+Console.WriteLine($"{(monblanc * 100.0 / total):f2}%");
+Console.WriteLine($"{(kilimanjaro * 100.0 / total):f2}%");
+Console.WriteLine($"{(k2 * 100.0 / total):f2}%");
+Console.WriteLine($"{(everest * 100.0 / total):f2}%");
